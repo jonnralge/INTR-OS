591c15d46f3b902529b9807bd8adbb2115401a96
seung0319 <nnn0319@naver.com>
2018-08-14 18:27:39 +0800

    fixed

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -8,12 +8,8 @@ public class GUI {
 
 	public static JFrame startFrame;
     public static JFrame trainFrame;
-<<<<<<< HEAD
     public static JFrame StrainFrame;
     
-=======
-
->>>>>>> branch 'Gui' of https://github.com/jonnralge/INTR-OS.git
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
         public void run() {
